c8c604118f777cdad7a120230c697320b25014e1
dorongruber <doron.gruber@gmail.com>
2023-11-14 14:59:41 +0200

    add skeleton loader style

--- src/app/components/max-min/max-min.component.ts
+++ src/app/components/max-min/max-min.component.ts
@@ -3,10 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-max-min',
   templateUrl: './max-min.component.html',
-  styleUrls: ['./max-min.component.scss']
+  styleUrls: ['./max-min.component.scss', '../../styles/skeleton.component.scss']
 })
 export class MaxMinComponent {
-  @Input() max!: number;
-  @Input() min!: number;
-  @Input() unit!: string;
+  @Input() max?: number;
+  @Input() min?: number;
+  @Input() unit?: string;
 }
--- src/app/components/weather-card/weather-card.component.ts
+++ src/app/components/weather-card/weather-card.component.ts
@@ -6,14 +6,14 @@ import { DailyWeather } from 'src/app/models/daily-weather';
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.scss']
+  styleUrls: ['./weather-card.component.scss', '../../styles/skeleton.component.scss']
 })
 export class WeatherCardComponent implements OnChanges {
   @Input() dailyWeather?: DailyWeather;
   @Input() temperatureUnit!: string;
   generalWeatherDescription?: string;
   baseIconPath: string = "assets/weather-icons/dovora/";
-  iconPath!: string;
+  iconPath?: string;
   ngOnChanges(changes: SimpleChanges): void {
     this.generalWeatherDescription = weatherCodes[this.dailyWeather!.weatherCode];
     this.iconPath = WeatherIcons[this.dailyWeather!.weatherCode];
