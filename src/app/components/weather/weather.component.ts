8c0394e5c05b7334f00009d6aad343e16e40a3f2
dorongruber <doron.gruber@gmail.com>
2023-11-14 11:06:01 +0200

    move managment of weather data from weather components to main component

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DailyWeather } from 'src/app/models/daily-weather';
 import { WeatherService } from 'src/app/services/weather.service';
 
@@ -7,32 +7,8 @@ import { WeatherService } from 'src/app/services/weather.service';
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.scss']
 })
-export class WeatherComponent implements OnInit {
+export class WeatherComponent {
   
-  weatherData: DailyWeather[] = [];
-  constructor(private weatherService: WeatherService) {
-    
-  }
-
-  ngOnInit(): void {
-    navigator.geolocation.getCurrentPosition(this.onWeatherResponse.bind(this));
-
-  }
-
-  async onWeatherResponse(position: GeolocationPosition) {
-    console.log("position ==> ", position);
-
-    const params = {
-      "latitude": position.coords.latitude,
-      "longitude": position.coords.longitude,
-      "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "apparent_temperature_max", "apparent_temperature_min", "precipitation_sum"],
-	    "timezone": "GMT"
-    };
-    
-    if(position) {
-      this.weatherData = await this.weatherService.getWeeklyWeather(params);
-      console.log("weatherData ==> ", this.weatherData);
-      
-    }
-  }
+  @Input() weatherData: DailyWeather[] = [];
+  @Input() temperatureUnit!: string;
 }
--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DailyWeather } from 'src/app/models/daily-weather';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-main',
@@ -7,6 +9,36 @@ import { Component } from '@angular/core';
 })
 export class MainComponent  {
 
-  
+  weatherData: DailyWeather[] = [];
+  params: any = {};
+  temperatureUnit: string = "c";
+  constructor(private weatherService: WeatherService) {
+    this.params = { 
+      "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "apparent_temperature_max", "apparent_temperature_min", "precipitation_sum"],
+	    "timezone": "GMT"
+    }
+  }
+
+  ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition(this.onWeatherResponse.bind(this));
+
+  }
+
+  async onValueChanges(params: any) {
+    for(let key of Object.keys(params)) {
+      this.params[key] = params[key];
+    }
+    
+    this.weatherData = await this.weatherService.getWeeklyWeather(this.params);
+    this.temperatureUnit = params["temperature_unit"][0];
+  }
+
+  async onWeatherResponse(position: GeolocationPosition) {    
+    if(position) {
+      this.params["latitude"] = position.coords.latitude;
+      this.params["longitude"] = position.coords.longitude;
+      this.weatherData = await this.weatherService.getWeeklyWeather(this.params);
+    }
+  }
 
 }
